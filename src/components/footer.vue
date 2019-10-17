4f1ea3ee1557a44da863ac977e782b416399d22c
rogersX <brianmawira2@gmail.com>
2019-10-17 13:16:02 +0300

    footer changes

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div absolute class="footer">
+    <div class="footer">
         <v-btn text color="#C0C0C0">
             <v-icon>mdi-arrow-left</v-icon>
             previous
@@ -10,4 +10,12 @@
         </v-btn>
     </div>
 </template>
+
+<style scoped>
+    .footer {
+        display: flex;
+        padding-left: 10px !important;
+        margin-top: 2% !important;
+    }
+</style>
 
